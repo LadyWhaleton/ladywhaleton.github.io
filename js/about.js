93d16e0d62bb725a97849b2ebaba3edde99df41b
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2016-10-03 21:42:35 -0700

    Finish styling of Awards section on About Page
    - Added CutieHack Logo, Sterling Highlander Logo
    - External links, Sterling Highlander contest entry

--- js/about.js
+++ js/about.js
@@ -2,13 +2,17 @@
 $(document).ready(function() {
 
 	// Work around to set divider widths according to text length. 
-	var subsectionNames = ["interests", "education", "skills", "orgs", "extras", "awards"];
+	var subsectionNames = ["bio", "interests", "education", "skills", "orgs", "extras", "awards"];
 
 	// Loop through array.
 	for (i = 0; i < subsectionNames.length; i++)
 	{
 		var idName = "#" + subsectionNames[i];
 		var width = $(idName).width();
+
+		if (idName =="#bio" || idName =="#orgs" || idName =="#extras")
+			width += 15;
+
 		$(idName + "-bg").css("width", width);
 	}
 
